1c4a844005d07d475f54077e2626963618ce0b66
F6323258 Lucas Pereira Gomes Linard <lucaslinard@bb.com.br>
2024-12-02 20:13:44 -0300

    inclui consumer

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,36 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text("Suas Notas"),
       ),
-      body: Placeholder(),
+      body: Consumer<DBProvider>(builder: (BuildContext context, DBProvider value, Widget? child) {
+        return FutureBuilder(
+          future: value.getNotes(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if(snapshot.connectionState == ConnectionState.done){
+              if(snapshot.data == Null){
+                return Center(
+                  child: Text("Nenhuma nota encontrada."),
+                );
+              }
+              else {
+                return ListView.builder(
+                  itemCount: snapshot.data?.length ?? 0,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text(snapshot.data[index]["title"]),
+                      subtitle: Text(snapshot.data[index]["body"]),
+                    );
+                  },
+                );
+              }
+            }
+            else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        );
+      },),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
           // Navegar para a criação de uma nota
